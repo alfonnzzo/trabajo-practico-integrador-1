a2e1964d76fd90a3bd5cc8f69a459cc15014d18b
unknown <alfonsozagana@gmail.com>
2025-09-04 15:17:38 -0300

    En proceso de desarrollar los modelos

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect:process.env.DB_DIALECT,
+});
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await sequelize.sync();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
